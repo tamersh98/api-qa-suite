244f1b6296193196c1007d4760d92b07668da912
Tamer Sheikh Ahmad <133818829+tamersh98@users.noreply.github.com>
2026-06-12 15:40:02 +0300

    Update api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,1 +1,11 @@
+import requests
 
+class APIClient:
+    def __init__(self):
+        self.base_url = "https://fakestoreapi.com"
+
+    def get_products(self):
+        return requests.get(f"{self.base_url}/products")
+
+    def create_product(self, payload):
+        return requests.post(f"{self.base_url}/products", json=payload)
